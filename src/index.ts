a7ab3790c03f360ff2243211d757b2826c133eed
Alicia Li <aliciay.li9@gmail.com>
2024-07-16 16:38:22 -0500

    added note-doodle component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { NoteExplorer } from './note-explorer';
 import { Piano } from '@tunepad/ui';
+import { NoteDoodle } from "./note-doodle" ;
+customElements.define(NoteDoodle.ELEMENT, NoteDoodle);
 customElements.define(Piano.ELEMENT, Piano);
 customElements.define(NoteExplorer.ELEMENT, NoteExplorer);
