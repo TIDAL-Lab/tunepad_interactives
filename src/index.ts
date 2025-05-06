0fe3ea4304dabced212d1a4f61b2567b477960b4
Michael Horn <michael-horn@northwestern.edu>
2025-05-05 23:02:17 -0500

    reorg code

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { NoteExplorer } from './note-explorer';
 import { Piano } from '@tunepad/ui';
 import { NoteDoodle } from "./note-doodle";
 import { Scale } from './scale';
-import { GrooveGarden } from './ggarden';
+import { GrooveGarden } from './ggarden/ggarden';
+import { NeuralNet } from './perceptron/perceptron';
 
 customElements.define(NoteDoodle.ELEMENT, NoteDoodle);
 customElements.define(Piano.ELEMENT, Piano);
 customElements.define(NoteExplorer.ELEMENT, NoteExplorer);
 customElements.define(Scale.ELEMENT, Scale);
 customElements.define(GrooveGarden.ELEMENT, GrooveGarden);
+customElements.define(NeuralNet.ELEMENT, NeuralNet);
