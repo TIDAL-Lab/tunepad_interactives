ad74bec0aac95197e4693e26fc002f8e996cd10d
Michael Horn <michael-horn@northwestern.edu>
2025-05-07 22:34:13 -0500

    removing perceptron code

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,9 @@ import { Piano } from '@tunepad/ui';
 import { NoteDoodle } from "./note-doodle";
 import { Scale } from './scale';
 import { GrooveGarden } from './ggarden/ggarden';
-import { NeuralNet } from './perceptron/perceptron';
 
 customElements.define(NoteDoodle.ELEMENT, NoteDoodle);
 customElements.define(Piano.ELEMENT, Piano);
 customElements.define(NoteExplorer.ELEMENT, NoteExplorer);
 customElements.define(Scale.ELEMENT, Scale);
 customElements.define(GrooveGarden.ELEMENT, GrooveGarden);
-customElements.define(NeuralNet.ELEMENT, NeuralNet);
